1a23d705be1384fd3cf2161dde4b15e088793636
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-05-23 09:58:09 -0500

    standardized and best practice on query and mutation input

--- src/organizations/organizations.resolver.ts
+++ src/organizations/organizations.resolver.ts
@@ -1,37 +1,39 @@
 import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { Organization } from './models/organization.entity';
 import { OrganizationsService } from './organizations.service';
-import { UseGuards, Logger } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { GqlUser } from 'src/common/decorators/gql-user.decorator';
 import { User } from 'src/users/models/user.entity';
+import { CreateOrganizationInput } from './models/create-organization.input';
+import { GetOrganizationByIdInput } from './models/get-organization-by-id.input';
 
 @Resolver(of => Organization)
 export class OrganizationsResolver {
-  private logger = new Logger(OrganizationsResolver.name);
   constructor(private organizationsService: OrganizationsService) {}
 
   @UseGuards(GqlAuthGuard)
   @Mutation(returns => Organization, { name: 'createOrganization' })
   async createOrganization(
-    @Args('name', { type: () => String }) name: string,
+    @Args('input') createOrganizationData: CreateOrganizationInput,
     @GqlUser() user: User,
   ) {
-    this.logger.log(JSON.stringify(user));
     const org = new Organization();
-    org.name = name;
+    org.name = createOrganizationData.name;
     return await this.organizationsService.create(org, user);
   }
 
   @UseGuards(GqlAuthGuard)
-  @Query(returns => [Organization], { name: 'organizations' })
+  @Query(returns => [Organization], { name: 'allOrganizations' })
   async getOrganizations() {
     return await this.organizationsService.findAll();
   }
 
   @UseGuards(GqlAuthGuard)
   @Query(returns => Organization, { name: 'organization' })
-  async getOrganizationById(@Args('id', { type: () => String }) id: string) {
-    return await this.organizationsService.findOne(id);
+  async getOrganizationById(
+    @Args('input') getOrganizationByIdData: GetOrganizationByIdInput,
+  ) {
+    return await this.organizationsService.findOne(getOrganizationByIdData.id);
   }
 }
