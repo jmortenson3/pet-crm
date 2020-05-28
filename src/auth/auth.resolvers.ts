1cdfc23cb44f9e8cfda286dcf8c3b1522ff2e642
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-05-27 21:34:55 -0500

    added input types to login and signup

--- src/auth/auth.resolvers.ts
+++ src/auth/auth.resolvers.ts
@@ -1,10 +1,12 @@
 import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { ConfigService } from '@nestjs/config';
-import { BadRequestException, Logger, NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { User } from 'src/users/models/user.entity';
 import { ResGql } from 'src/common/decorators/res-gql.decorator';
 import { Response } from 'express';
+import { LoginInput } from './models/login.input';
+import { SignupInput } from './models/signup.input';
 
 @Resolver(of => User)
 export class AuthResolver {
@@ -14,10 +16,8 @@ export class AuthResolver {
   ) {}
 
   @Mutation(returns => User, { name: 'signup' })
-  async signup(
-    @Args({ name: 'username', type: () => String }) username: string,
-    @Args({ name: 'password', type: () => String }) password: string,
-  ) {
+  async signup(@Args('input') signupData: SignupInput) {
+    const { username, password } = signupData;
     if (!username || !password) {
       throw new BadRequestException();
     }
@@ -25,11 +25,11 @@ export class AuthResolver {
   }
 
   @Mutation(returns => User, { name: 'login' })
-  async login(
-    @Args({ name: 'username', type: () => String }) username: string,
-    @Args({ name: 'password', type: () => String }) password: string,
-    @ResGql() res: Response,
-  ) {
+  async login(@Args('input') loginData: LoginInput, @ResGql() res: Response) {
+    const { username, password } = loginData;
+    if (!username || !password) {
+      throw new BadRequestException();
+    }
     const user = await this.authService.validateUser(username, password);
 
     if (!user) {
@@ -41,7 +41,6 @@ export class AuthResolver {
     res.cookie(this.configService.get<string>('JWT_COOKIE_NAME'), token, {
       //httpOnly: true,  re-enable this
     });
-
     return user;
   }
 }
